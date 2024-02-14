0241512ec81ffd185856489e8566a0d14a6f1ad6
Gabriel-Comisaru <comisaru.gabriel@gmail.com>
2024-02-14 20:01:47 +0200

    added build

---
+++ gfx-framework-master/build/Tema1.cpp
@@ -0,0 +1,16 @@
+#include "Tema1.h"
+
+using namespace std;
+using namespace m1;
+
+Tema1::Tema1()
+{
+}
+
+
+Tema1::~Tema1()
+{
+}
+
+
+void Tema1::Init(){}
---
+++ gfx-framework-master/build/Tema1.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "components/simple_scene.h"
+
+namespace m1
+{
+	class Tema1 : public gfxc::SimpleScene
+	{
+		public:
+			Tema1();
+			~Tema1();
+
+		void Init() override;
+	};
+}
